65b0c861a578508b1df165acab89feded1c1c508
binkokiat <binkokiat@gmail.com>
2023-08-10 23:32:16 +0700

    Add all file f Project
    https://school.borntodev.com/course/introduction-to-web-app-development-with-nodejs-express/fd40e626-1b84-4972-bf6a-76e012b608d7

---
+++ EP4/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const debug = require('debug')('app');
+// import chalk from 'chalk';
+// const chalk = require('chalk')
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) =>{
+
+    res.send('Hello Growth Potential !!');
+
+})
+
+app.listen(port, ()=>{
+
+    // console.log("Listening on port %d", port);
+    // console.log("Listening on port" + chalk.green(" : " + port));
+    console.log("Listening on port" + " : " + port);
+    // debug("Listening on port" + " : " + port);
+
+})
